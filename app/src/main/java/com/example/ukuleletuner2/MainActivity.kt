53d399c50e5fc02e7d9702f0d05cac052b51662d
Matej Martinický <martinicky3@stud.uniza.sk>
2025-05-29 10:25:40 +0200

    adding theming to the app
    tutorial: https://developer.android.com/codelabs/jetpack-compose-theming#3

--- app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
+++ app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,13 +23,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.example.ukuleletuner2.ui.theme.AppTheme
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Request microphone permission
+        //mic permission
         ActivityCompat.requestPermissions(
             this,
             arrayOf(Manifest.permission.RECORD_AUDIO),
@@ -32,7 +38,11 @@ class MainActivity : ComponentActivity() {
         )
 
         setContent {
-            Navigation()
+            AppTheme {
+                Surface(tonalElevation = 5.dp) {
+                    Navigation()
+                }
+            }
         }
     }
 }
