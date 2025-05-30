34cfd17fd8f3856499fb739b789bb24904f66089
Matej Martinický <martinicky3@stud.uniza.sk>
2025-05-30 12:51:14 +0200

    commenting on language switching

--- app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
+++ app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
@@ -12,7 +12,7 @@ import com.example.ukuleletuner2.viewModels.themeViewModel.ThemeViewModel
 import com.example.ukuleletuner2.ui.theme.AppTheme
 import com.example.ukuleletuner2.ui.theme.ColorThemes
 
-//comenting on main
+//comenting on language-switcing
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
