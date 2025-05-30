00df04b9e7211905e53aa9785a4094d3eaa25bb5
Matej Martinický <martinicky3@stud.uniza.sk>
2025-05-30 12:58:20 +0200

    main comment

--- app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
+++ app/src/main/java/com/example/ukuleletuner2/MainActivity.kt
@@ -13,6 +13,7 @@ import com.example.ukuleletuner2.ui.theme.AppTheme
 import com.example.ukuleletuner2.ui.theme.ColorThemes
 
 //comenting on main
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
