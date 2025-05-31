28868702e47a4da47eea9fe2be7aa7da5c9260da
Matej Martinický <martinicky3@stud.uniza.sk>
2025-05-31 09:35:00 +0200

    separating behaviour from settings related UI to SettingViewModel for better alignment with principals

--- app/src/main/java/com/example/ukuleletuner2/ui/components/cards/ThemeCard.kt
+++ app/src/main/java/com/example/ukuleletuner2/ui/components/cards/ThemeCard.kt
@@ -18,19 +18,8 @@ import com.example.ukuleletuner2.viewModels.themeViewModel.ThemeViewModel
 import com.example.ukuleletuner2.ui.theme.ColorThemes
 import androidx.compose.foundation.lazy.items
 
-fun getThemeImageResource(theme: ColorThemes): Int { //to view model
-    return when (theme) {
-        ColorThemes.White -> R.drawable.theme_white
-        ColorThemes.Orange -> R.drawable.theme_orange
-        ColorThemes.Green -> R.drawable.theme_green
-        ColorThemes.Black -> R.drawable.theme_black
-    }
-}
-
 @Composable
 fun ThemeCard(themeViewModel: ThemeViewModel) {
-    val themes = ColorThemes.entries
-
     Column {
         Text(
             text = stringResource(R.string.theme_color),
@@ -42,9 +31,9 @@ fun ThemeCard(themeViewModel: ThemeViewModel) {
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.padding(vertical = 8.dp)
         ) {
-            items(themes) { theme ->
+            items(themeViewModel.themes) { theme ->
                 ThemeImage(
-                    themePainter = painterResource(id = getThemeImageResource(theme)),
+                    themePainter = painterResource(id = themeViewModel.getThemeImageResource(theme)),
                     size = 60.dp,
                     onClick = {
                         themeViewModel.updateTheme(theme)
--- app/src/main/java/com/example/ukuleletuner2/viewModels/SettingsViewModel/SettingsViewModel.kt
+++ app/src/main/java/com/example/ukuleletuner2/viewModels/SettingsViewModel/SettingsViewModel.kt
@@ -4,12 +4,21 @@ import android.app.Activity
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.ukuleletuner2.R
+import com.example.ukuleletuner2.ui.components.cards.Language
 import com.example.ukuleletuner2.utility.updateLocale
 
 
 class SettingsViewModel : ViewModel() {
     var language = mutableStateOf("en")
         private set
+
+    val languages = listOf(
+        Language("en", R.drawable.flag_usa),
+        Language("sk", R.drawable.flag_slovak),
+        Language("de", R.drawable.flag_german),
+        Language("es", R.drawable.flag_spanish)
+    )
 
     fun setLanguage(lang: String) {
         language.value = lang
