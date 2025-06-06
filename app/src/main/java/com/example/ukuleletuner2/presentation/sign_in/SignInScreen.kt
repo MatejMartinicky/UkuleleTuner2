9ab320eba9c42c9d4992de8588f7dd04605e713c
Matej Martinický <martinicky3@stud.uniza.sk>
2025-06-06 12:44:31 +0200

    button UI redesign

--- app/src/main/java/com/example/ukuleletuner2/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/ukuleletuner2/presentation/sign_in/SignInScreen.kt
@@ -1,19 +1,38 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.ukuleletuner2.presentation.sign_in
 
 //https://www.youtube.com/watch?v=zCIfBbm06QM (full)
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.ukuleletuner2.R
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.example.ukuleletuner2.ui.components.buttons.GoogleLogoButton
 
 @Composable
 fun SignIScreen(
@@ -30,14 +49,56 @@ fun SignIScreen(
             ).show()
         }
     }
+
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize()
     ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "sign in")
+        Image(
+            painter = painterResource(id = R.drawable.sing_in_image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(0.3f))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo_high_resolution),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(0.4f))
+
+            Text(
+                text = stringResource(R.string.sign_in_prompt),
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            Spacer(modifier = Modifier.weight(0.3f))
+
+            Text(
+                text = stringResource(R.string.sign_in_disclamer),
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            Spacer(modifier = Modifier.weight(0.1f))
+
+            GoogleLogoButton(
+                text = "sign in",
+                onClick = onSignInClick)
         }
     }
 }
