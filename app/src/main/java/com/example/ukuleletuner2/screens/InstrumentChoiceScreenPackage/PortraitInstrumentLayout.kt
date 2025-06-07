f35071a30c910a02266261e12b4ccecef7cc2206
Matej Martinický <martinicky3@stud.uniza.sk>
2025-06-07 19:50:35 +0200

    spliting instrument choice screen

---
+++ app/src/main/java/com/example/ukuleletuner2/screens/InstrumentChoiceScreenPackage/PortraitInstrumentLayout.kt
@@ -0,0 +1,37 @@
+package com.example.ukuleletuner2.screens.InstrumentChoiceScreenPackage
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+internal fun PortraitInstrumentLayout(onNavigateToTunerScreen: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primaryContainer
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            InstrumentChoiceTitle()
+
+            Spacer(modifier = Modifier.height(48.dp))
+
+            InstrumentSelector(onNavigateToTunerScreen)
+
+            Spacer(modifier = Modifier.height(48.dp))
+
+        }
+    }
+}
